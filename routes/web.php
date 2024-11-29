ee08be959ba7c4951167297bcb20c4e98df5ad61
Harris <harris.git21@gmail.com>
2024-11-29 17:47:41 +0700

    skydash initial

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.user-pages.login.index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/forms', function () {
+    return view('pages.forms.index');
+});
+
+Route::get('/buttons', function () {
+    return view('pages.ui-features.buttons.index');
+});
+
+Route::get('/dropdowns', function () {
+    return view('pages.ui-features.dropdowns.index');
+});
+
+Route::get('/typography', function () {
+    return view('pages.ui-features.typography.index');
+});
+
+Route::get('/chart', function () {
+    return view('pages.chart.index');
+});
+
+Route::get('/table', function () {
+    return view('pages.table.index');
+});
+
+Route::get('/icons', function () {
+    return view('pages.icons.index');
+});
+
+Route::get('/login', function () {
+    return view('pages.user-pages.login.index');
+});
+
+Route::get('/register', function () {
+    return view('pages.user-pages.register.index');
+});
+
+Route::get('/erro404', function () {
+    return view('pages.error-pages.404.index');
+});
+
+Route::get('/erro500', function () {
+    return view('pages.error-pages.500.index');
+});
