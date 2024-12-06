da2b920e02c27d44cf59ca663e6b002d7dfbea31
Harris <harris.git21@gmail.com>
2024-12-06 17:47:47 +0700

    fix: navbar JVCKENWOOD

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,14 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-      <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="{{ asset('assets/images/logo.svg') }}" class="mr-2" alt="logo"/></a>
-      <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('assets/images/logo-mini.svg') }}" alt="logo"/></a>
+      {{-- <div class="d-flex align-items-center justify-content-between"> --}}
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <span class="d-none d-lg-block">JVCKENWOOD</span>
+      </a>
+      {{-- <i class="bi bi-list toggle-sidebar-btn"></i> --}}
+    {{-- </div><!-- End Logo --> --}}
+      {{-- <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="{{ asset('assets/images/logo.svg') }}" class="mr-2" alt="logo"/></a> --}}
+      {{-- <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('assets/images/logo-mini.svg') }}" alt="logo"/></a> --}}
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
       <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
